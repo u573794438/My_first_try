a8a522104e38872c6bce2f2198f331726587ae1d
u573794438 <573794438@qq.com>
2025-06-12 21:33:44 +0800

    feat: 添加未找到页面组件

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '24px' }}>
+      页面未找到
+    </div>
+  );
+};
+
+export default NotFound;
